780849b976dce48f20e309ec17e6141aeaadc342
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2022-10-22 15:15:29 -0300

    2º Changing calculator tests

--- wesley/salary-calculator/README.md
+++ wesley/salary-calculator/README.md
@@ -12,3 +12,12 @@ Deve-se implementar uma calculadora de salário de funcionários. Um funcionári
 
 
 - Caso o cargo seja GERENTE, o funcionário terá desconto de 30% caso o salário seja maior ou igual que 5.000,00, ou apenas 20% caso o salário seja menor que isso.
+
+#### Usando a calculadora
+
+Para usar a calculadora, primeiro importe a classe `SalaryCalculator` e faça:
+
+```java
+Manager manager = new Manager('Felipe', 'felipe@gmail.com', 3000)
+SalaryCalculator.getSalary(manager); // 3000 * 0.8 = 2400
+```
--- wesley/salary-calculator/src/test/java/com/tdd/CalculatorTest.java
+++ wesley/salary-calculator/src/test/java/com/tdd/CalculatorTest.java
@@ -4,30 +4,22 @@ import com.tdd.positions.Dba;
 import com.tdd.positions.Developer;
 import com.tdd.positions.Manager;
 import com.tdd.positions.Tester;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
-    SalaryCalculator calculator = null;
-
-    @BeforeEach
-    public void init(){
-        calculator = new SalaryCalculator();
-    }
-
     @Test
     @DisplayName("Get dev salary")
     void whenDevGetSalaryThenSalaryIsRetrieved() {
         // base salary >= 3000
         Developer auxDev = new Developer("Wesley Santos", "wesley123@gmail.com", 3000.0);
-        assertEquals(calculator.getSalary(auxDev), auxDev.getBaseSalary() * 0.8);
+        assertEquals(SalaryCalculator.getSalary(auxDev), auxDev.getBaseSalary() * 0.8);
 
         // base salary < 3000
         auxDev = new Developer("Wesley Santos", "wesley123@gmail.com", 2000.0);
-        assertEquals(calculator.getSalary(auxDev), auxDev.getBaseSalary() * 0.9);
+        assertEquals(SalaryCalculator.getSalary(auxDev), auxDev.getBaseSalary() * 0.9);
     }
 
     @Test
@@ -35,11 +27,11 @@ public class CalculatorTest {
     void whenDbaGetSalaryThenSalaryIsRetrieved() {
         // base salary >= 2000
         Dba auxDba = new Dba("Wesley Santos", "wesley123@gmail.com", 3000.0);
-        assertEquals(calculator.getSalary(auxDba), auxDba.getBaseSalary() * 0.75);
+        assertEquals(SalaryCalculator.getSalary(auxDba), auxDba.getBaseSalary() * 0.75);
 
         // base salary < 2000
         auxDba = new Dba("Wesley Santos", "wesley123@gmail.com", 1500.0);
-        assertEquals(calculator.getSalary(auxDba), auxDba.getBaseSalary() * 0.85);
+        assertEquals(SalaryCalculator.getSalary(auxDba), auxDba.getBaseSalary() * 0.85);
     }
 
     @Test
@@ -47,11 +39,11 @@ public class CalculatorTest {
     void whenTesterGetSalaryThenSalaryIsRetrieved() {
         // base salary >= 2000
         Tester auxTester = new Tester("Wesley Santos", "wesley123@gmail.com", 3000.0);
-        assertEquals(calculator.getSalary(auxTester), auxTester.getBaseSalary() * 0.75);
+        assertEquals(SalaryCalculator.getSalary(auxTester), auxTester.getBaseSalary() * 0.75);
 
         // base salary < 2000
         auxTester = new Tester("Wesley Santos", "wesley123@gmail.com", 1500.0);
-        assertEquals(calculator.getSalary(auxTester), auxTester.getBaseSalary() * 0.85);
+        assertEquals(SalaryCalculator.getSalary(auxTester), auxTester.getBaseSalary() * 0.85);
     }
 
     @Test
@@ -59,10 +51,10 @@ public class CalculatorTest {
     void whenManagerGetSalaryThenSalaryIsRetrieved() {
         // base salary >= 5000
         Manager auxManager = new Manager("Wesley Santos", "wesley123@gmail.com", 5000.0);
-        assertEquals(calculator.getSalary(auxManager), auxManager.getBaseSalary() * 0.7);
+        assertEquals(SalaryCalculator.getSalary(auxManager), auxManager.getBaseSalary() * 0.7);
 
         // base salary < 5000
         auxManager = new Manager("Wesley Santos", "wesley123@gmail.com", 3500.0);
-        assertEquals(calculator.getSalary(auxManager), auxManager.getBaseSalary() * 0.8);
+        assertEquals(SalaryCalculator.getSalary(auxManager), auxManager.getBaseSalary() * 0.8);
     }
 }
