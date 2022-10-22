c422d48852dad347e2eedb44e63f4c0f1ed1f187
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2022-10-22 15:10:33 -0300

    Test green bar

---
+++ wesley/salary-calculator/src/main/java/com/tdd/SalaryCalculator.java
@@ -0,0 +1,9 @@
+package com.tdd;
+
+import com.tdd.interfaces.Salaried;
+
+public class SalaryCalculator {
+    public Double getSalary(Salaried salaried) {
+        return salaried.getSalary();
+    }
+}
