d6d82ec7c0bf4f72b8a54f059e5906aedf6c5bce
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2022-10-21 21:42:51 -0300

    First employee test

---
+++ wesley/salary-calculator/src/test/java/com/tdd/EmployeeTest.java
@@ -0,0 +1,25 @@
+package com.tdd;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmployeeTest {
+    Employee employee = null;
+
+    @BeforeEach
+    public void init(){
+        employee = new Employee("Wesley Santos", "wesley123@gmail.com", 6000.0);
+    }
+
+    @Test
+    @DisplayName("Employee data is retrievable")
+    void whenEmployeeIsCreatedThenAllDataIsRetrievable() {
+        assertEquals(employee.getName(), "Wesley Santos");
+        assertEquals(employee.getEmail(), "wesley123@gmail.com");
+        assertEquals(employee.getBaseSalary(), 6000);
+        assertEquals(employee.getPosition(), Position.DEVELOPER);
+    }
+}
